bdf0cf1a4b1b8f472cdb990134fbf8976fb9c79c
nina <ninagbs@abv.bg>
2024-10-23 13:04:10 +0300

    added more quesions

--- src/data/quiz3.js
+++ src/data/quiz3.js
@@ -1,16 +1,163 @@
+// Requirement analysis. Use Case Diagrams. Relationships
 export const mysql3 = [
   {
     index: 1,
-    question: "What is the primary purpose of requirements analysis?",
+    question: "Which of the following is NOT a domain depicted in requirements models (also called analysis models)?",
     choices: [
-      "To define the technical specifications of the system",
-      "To identify and document user needs and system requirements",
-      "To design the user interface",
-      "To write code for the system"
+      "The information domain",
+      "The functional domain",
+      "The behavioral domain",
+      "The architectural domain"
     ],
-    correctAnswer: "To identify and document user needs and system requirements",
+    correctAnswer: "The architectural domain",
     imageURL: null
   },
+  {
+    index: 2,
+    question: "What do design models in software engineering represent?",
+    choices: [
+      "The software architecture, the user interface, and component-level details that help practitioners construct the software effectively.",
+      "Customer requirements across the information, functional, and behavioral domains.",
+      "Only the user interface of the software system.",
+      "The deployment process of the software on different environments."
+    ],
+    correctAnswer: "The software architecture, the user interface, and component-level details that help practitioners construct the software effectively.",
+    imageURL: null
+  },
+  {
+    index: 3,
+    question: "In software engineering, two classes of models can be created. Which is not?",
+    choices: [
+      "Requirements models (also called analysis models)",
+      "Design models",
+      "Information customer models"
+    ],
+    correctAnswer: "Information customer models",
+    imageURL: null
+  },
+  {
+    index: 4,
+    question: "Which of the following is NOT one of the principles of requirements modeling?",
+    choices: [
+      "The information domain of a problem must be represented and understood.",
+      "The functions that the software performs must be ignored.",
+      "The functions that the software performs must be defined.",
+      "The behavior of the software consequence of external events.",
+      "The models that describe information, function, and behavior must be partitioned in a manner that shows detail in a layered (or hierarchical) fashion.",
+      "The analysis task should move from essential information toward implementation detail."
+    ],
+    correctAnswer: "The functions that the software performs must be ignored.",
+    imageURL: null
+  },
+  {
+    index: 5,
+    question: "Which of the following statements about requirements is NOT true?",
+    choices: [
+      "A requirement may range from a high-level abstract statement to a detailed mathematical functional specification.",
+      "Requirements may serve a dual function as the basis for a contract and must be strictly defined with no room for interpretation.",
+      "Both high-level and detailed specifications may be called requirements.",
+      "Requirements can be open to interpretation if they serve as the basis for a bid for a contract."
+    ],
+    correctAnswer: "Requirements may serve a dual function as the basis for a contract and must be strictly defined with no room for interpretation.",
+    imageURL: null
+  },
+  {
+    index: 6,
+    question: "Which of the following is NOT true about user requirements?",
+    choices: [
+      "User requirements are statements in natural language and may include diagrams.",
+      "User requirements are primarily written for developers and operations teams.",
+      "User requirements describe the services the system provides and its operational constraints.",
+      "User requirements are written for customers."
+    ],
+    correctAnswer: "User requirements are primarily written for developers and operations teams.",
+    imageURL: null
+  },
+  {
+    index: 7,
+    question: "Which of the following statements about system requirements is NOT accurate?",
+    choices: [
+      "System requirements are a structured document detailing the system’s functions and operational constraints.",
+      "System requirements define what should be implemented and may be part of a contract.",
+      "System requirements are primarily written for customers to understand the system's capabilities.",
+      "System requirements are focused on what the system must do and Written for dev-op’s."
+    ],
+    correctAnswer: "System requirements are primarily written for customers to understand the system's capabilities.",
+    imageURL: null
+  },
+  {
+    index: 8,
+    question: "Which of the following is NOT true about functional requirements?",
+    choices: [
+      "Functional requirements state the services the system should provide.",
+      "Functional requirements describe how the system should react to particular inputs.",
+      "Functional requirements are constraints on the system's overall performance.",
+      "Functional requirements may also state what the system should not do."
+    ],
+    correctAnswer: "Functional requirements are constraints on the system's overall performance.",
+    imageURL: null
+  },
+  {
+    index: 9,
+    question: "Which of the following statements about non-functional requirements is NOT accurate?",
+    choices: [
+      "Non-functional requirements impose constraints on the services offered by the system.",
+      "Non-functional requirements often apply to the system as a whole rather than individual features.",
+      "Non-functional requirements may include timing constraints and standards.",
+      "Non-functional requirements are solely focused on what the system should do."
+    ],
+    correctAnswer: "Non-functional requirements are solely focused on what the system should do.",
+    imageURL: null
+  },
+  {
+    index: 10,
+    question: "Which of the following is NOT considered a subset of functional requirements?",
+    choices: [
+      "Domain requirements",
+      "Constraints on the system from the domain of operation",
+      "Timing constraints",
+      "Statements of services the system should provide"
+    ],
+    correctAnswer: "Timing constraints",
+    imageURL: null
+  },
+  {
+    index: 11,
+    question: "Which of the following statements about functional requirements is NOT true?",
+    choices: [
+      "Functional requirements describe the functionality or services provided by the system.",
+      "Functional requirements are independent of the type of software and expected users.",
+      "Functional user requirements may be high-level statements of what the system should do.",
+      "Functional system requirements should describe the system services in detail.",
+      "Depend on the type of software, expected users and the type of system where the software is used."
+    ],
+    correctAnswer: "Functional requirements are independent of the type of software and expected users.",
+    imageURL: null
+  },
+  {
+    index: 12,
+    question: "Which of the following is NOT a characteristic of requirements completeness and consistency?",
+    choices: [
+      "Requirements should include descriptions of all necessary facilities - Complete",
+      "Requirements should be free of conflicts or contradictions- Consistent",
+      "Requirements should not be both - complete and consistent.",
+
+    ],
+    correctAnswer: "Requirements should not be both - complete and consistent.",
+    imageURL: null
+  },
+  {
+    index: 13,
+    question: "Which of the following is NOT true regarding the consistency of requirements?",
+    choices: [
+      "There should be no conflicts or contradictions in the descriptions of system facilities.",
+      "There should be conflicts or contradictions in the descriptions of system facilities.",
+
+    ],
+    correctAnswer: "There should be conflicts or contradictions in the descriptions of system facilities.",
+    imageURL: null
+  }
+
 
 
 
