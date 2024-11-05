aa23f2a4283a3ef051a2764a6d49ce6fb681cfdf
Nina Nikolova <40785979+NinaNikolova@users.noreply.github.com>
2024-11-05 20:12:54 +0200

    Update QuizSelector.jsx

--- src/components/Quiz1/QuizSelector.jsx
+++ src/components/Quiz1/QuizSelector.jsx
@@ -6,13 +6,13 @@ export default function QuizSelector({ selectedQuiz, handleQuizChange }) {
                 <option value="mysql2">OO Modelling with UML </option>
                 <option value="mysql3">Requirements analysis. Use case diagrams</option>
                 <option value="mysql4">OO analysis - class, object and profile diagrams</option>
-                <option value="mysql5">OO Design: Aggregation, Composition, Inheritance...</option>
+                <option value="mysql5">OO Design: Aggregation, Composition, Inheritance</option>
                 <option value="mysql6">Interaction diagrams</option>
-                <option value="mysql7">Behavior.Structure.Activity/State Machine Diagrams</option>
-                <option value="mysql8">Нововъведения в UML 2.*.OCL. MDA. Времеви диаграми </option>
-                <option value="mysql9">Диаграми на компонентите и на внедряването </option>
-                <option value="mysql10">Проектиране на софтуерната архитектура</option>
-                <option value="mysql11">Построяване на итерациите. Право и реверсивно проектиране. </option>
+                <option value="mysql7">Behavior.Structure.Activity/State Machine</option>
+                <option value="mysql8">Нововъведения в UML 2.*.OCL. MDA. </option>
+                <option value="mysql9">Диаграми на комп. и на внедряването </option>
+                <option value="mysql10">Проектиране на софт. архитектура</option>
+                <option value="mysql11">Построяване на итерациите. </option>
                 <option value="mysql12">ExamPreparation</option>
                 
             </select>
