23ef1cb641edd00b66a15d3eda37fb312b84fcd0
nina <nina_nikolova@students.softuni.bg>
2024-11-25 12:51:33 +0200

    added questions

--- src/data/quiz9.js
+++ src/data/quiz9.js
@@ -1,15 +1,255 @@
 export const mysql9 = [
   {
     index: 1,
-    question: "What is the primary purpose of activity diagrams in UML?",
+    question: "What is the primary purpose of a component diagram in UML?",
     choices: [
-      "To describe the static structure of a system.",
-      "To represent the dynamic aspects of a system through workflows.",
-      "To show the relationships between classes.",
-      "To define the architecture of a system."
+      "To show the dynamic behavior of objects.",
+      "To represent the logical organization and relationships of components.",
     ],
-    correctAnswer: "To represent the dynamic aspects of a system through workflows.",
+    correctAnswer: "To represent the logical organization and relationships of components.",
     imageURL: null
+  },
+  {
+    index: 2,
+    question: "What does an assembly connector in a component diagram represent?",
+    choices: [
+      "A connection between hardware nodes.",
+      "A bridge between a required interface and a provided interface of components. The assembly connector allows one component to provide the services (the boll) that another component requires (the socket).",
+    ],
+    correctAnswer: "A bridge between a required interface and a provided interface of components. The assembly connector allows one component to provide the services (the boll) that another component requires (the socket).",
+    imageURL: null
+  },
+  {
+    index: 3,
+    question: "What is a deployment diagram primarily used for?",
+    choices: [
+      "To represent the physical deployment of software components on hardware nodes.",
+      "To define interfaces for classes and objects.",
+    ],
+    correctAnswer: "To represent the physical deployment of software components on hardware nodes.",
+    imageURL: "https://online.visual-paradigm.com/images/tutorials/deployment-diagram-tutorial/04-deployment-diagram-node-and-artifacts.png"
+  },
+  {
+    index: 4,
+    question: "Which of the following is NOT a typical element in a deployment diagram?",
+    choices: [
+      "Nodes",
+      "Artifacts",
+      "Interfaces",
+      "Components"
+    ],
+    correctAnswer: "Interfaces",
+    imageURL: null
+  },
+  {
+    index: 5,
+    question: "In UML, what does a port represent in component diagrams?",
+    choices: [
+      "A physical connection between two nodes.",
+      "A bi-directional interface for communication.",
+    ],
+    correctAnswer: "A bi-directional interface for communication.",
+    imageURL: "https://media.geeksforgeeks.org/wp-content/uploads/20240531161233/4.png"
+  },
+  {
+    index: 6,
+    question: "What is NOT true about a node represent in deployment diagrams?",
+    choices: [
+      "It is a logical grouping of object classes.",
+      "A software or hardware resource capable of hosting software.",
+      "Its symbol - 3D boxes",
+      "It provides context for deployment, showing where components reside and execute within the system’s infrastructure."
+    ],
+    correctAnswer: "It is a logical grouping of object classes.",
+    imageURL: "https://media.geeksforgeeks.org/wp-content/uploads/20240531154417/nodes.png"
+  },
+  {
+    index: 7,
+    question: "What is an example of a deliverable component / delivered to customers/?",
+    choices: [
+      "Source code files",
+      "Executables",
+      "Database tables",
+      "All of the above"
+    ],
+    correctAnswer: "Executables",
+    imageURL: null
+  },
+  {
+    index: 8,
+    question: "Which stereotype is commonly associated with components?",
+    choices: [
+      "<<node>>",
+      "<<component>>",
+      "<<artifact>>",
+      "<<port>>"
+    ],
+    correctAnswer: "<<component>>",
+    imageURL: "https://media.geeksforgeeks.org/wp-content/uploads/20240531160215/1drawio.png"
+  },
+  {
+    index: 9,
+    question: "What is the focus of component diagrams in UML?",
+    choices: [
+      "Logical relationships between components.",
+      "Dynamic behavior of the system.",
+
+    ],
+    correctAnswer: "Logical relationships between components.",
+    imageURL: null
+  },
+  {
+    index: 10,
+    question: "What does a dependency in a component diagram indicate?",
+    choices: [
+      "A hierarchical structure among components.",
+      "A component requires another for compilation or execution.",
+    ],
+    correctAnswer: "A component requires another for compilation or execution.",
+    imageURL: null
+  },
+  {
+    index: 11,
+    question: "What is a key distinction between component and deployment diagrams?",
+    choices: [
+      "Component diagrams are used during the design phase, while deployment diagrams are used for physical deployment.",
+      "There is no distinction; they serve the same purpose."
+    ],
+    correctAnswer: "Component diagrams are used during the design phase, while deployment diagrams are used for physical deployment.",
+    imageURL: null
+  },
+  {
+    index: 12,
+    question: "What does a provided interface signify in UML component diagrams?",
+    choices: [
+      "An interface that a component offers.",
+      "An interface that a component requires.",
+    ],
+    correctAnswer: "An interface that a component offers.",
+    imageURL: "https://media.geeksforgeeks.org/wp-content/uploads/20240531160749/2.png"
+  },
+  {
+    index: 13,
+    question: "What is NOT true about deployment diagram's artifact?",
+    choices: [
+      "Rectangles with the artifact stereotype («artifact»)",
+      "They represent a physical piece of software or data deployed to a node.",
+      "Show how software artifacts, like executables or data files, relate to the components",
+      "To define the logical grouping of object classes."
+    ],
+    correctAnswer: "To define the logical grouping of object classes.",
+    imageURL: "https://media.geeksforgeeks.org/wp-content/uploads/20240531161438/5.png"
+  },
+  {
+    index: 14,
+    question: "What type of process scheduling can a processor specify in deployment diagrams?",
+    choices: [
+      "Preemptive",
+      "Non-preemptive",
+      "Cyclic",
+      "All of the above"
+    ],
+    correctAnswer: "All of the above",
+    imageURL: null
+  },
+  {
+    index: 15,
+    question: "What is the purpose of a component's stereotype?",
+    choices: [
+      "To specify its physical location.",
+      "To indicate its type or role in the model.",
+    ],
+    correctAnswer: "To indicate its type or role in the model.",
+    imageURL: null
+  },
+  {
+    index: 16,
+    question: "In component diagrams, what is an example of a stereotype?",
+    choices: [
+      "<<component>>",
+      "<<interface>>",
+      "<<port>>",
+      "All of the above"
+    ],
+    correctAnswer: "<<component>>",
+    imageURL: null
+  },
+  {
+    index: 17,
+    question: "What is a hardware node in deployment diagrams?",
+    choices: [
+      "A representation of system functionality.",
+      "A physical entity hosting software components.",
+    ],
+    correctAnswer: "A physical entity hosting software components.",
+    imageURL: null
+  },
+  {
+    index: 18,
+    question: "Which of the following is true for component packages?",
+    choices: [
+      "They represent hardware dependencies.",
+      "They group logically related components.",
+
+    ],
+    correctAnswer: "They group logically related components.",
+    imageURL: null
+  },
+  {
+    index: 19,
+    question: "What does a state diagram in UML describe?",
+    choices: [
+      "Physical deployment of nodes.",
+      "Dynamic transitions between states of an object.",
+    ],
+    correctAnswer: "Dynamic transitions between states of an object.",
+    imageURL: null
+  },
+  {
+    index: 20,
+    question: "What is a key focus of deployment diagrams?",
+    choices: [
+      "Logical organization of software modules.",
+      "Physical allocation of software to hardware nodes.",
+    ],
+    correctAnswer: "Physical allocation of software to hardware nodes.",
+    imageURL: null
+  },
+  {
+    index: 21,
+    question: "A component diagram has a higher level of abstraction than a Class Diagram - usually a component is implemented by one or more classes. Is this true?",
+    choices: [
+      "Yes",
+      "No",
+    ],
+    correctAnswer: "Yes",
+    imageURL: null
+  },
+  {
+    index: 22,
+    question: "Which is not a relationship between components and interfaces?",
+    choices: [
+      "Dependency (dashed arrow) -  Indicates that one component relies on another",
+      "Association (solid line) - Shows a more permanent relationship between components",
+      "Assembly connector - Connects a required interface of one component to a provided interface of another",
+      "Nodes"
+    ],
+    correctAnswer: "Nodes",
+    imageURL: "https://media.geeksforgeeks.org/wp-content/uploads/20240531161020/3.png"
+  },
+  {
+    index: 23,
+    question: "Which is NOT a key concept of deplpyment diagram?",
+    choices: [
+      "Dependency",
+      "Association",
+      "Interface",
+      "Node",
+      "Component",
+      "Artifact"
+    ],
+    correctAnswer: "Class",
+    imageURL: "https://support.content.office.net/en-us/media/257901c1-24a3-41f5-bca4-7b9f8fe95364.png"
   },
 
 ];
