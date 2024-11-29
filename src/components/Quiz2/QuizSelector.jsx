9c09c6ac9e45605c6c570e829a191743b1c4c7ac
nina <nina_nikolova@students.softuni.bg>
2024-11-29 10:11:52 +0200

    added symmary

--- src/components/Quiz2/QuizSelector.jsx
+++ src/components/Quiz2/QuizSelector.jsx
@@ -1,4 +1,36 @@
+import { useState, useEffect } from 'react';
+import { FaDownload } from 'react-icons/fa';
 export default function QuizSelector({ selectedQuiz, handleQuizChange }) {
+    const [quizFile, setQuizFile] = useState("/mysql21.pdf");
+
+    // Use an effect to update quizFile when selectedQuiz changes
+    useEffect(() => {
+      switch (selectedQuiz) {
+        case 'mysql1':
+          setQuizFile("/mysql21.pdf");
+          break;
+        case 'mysql2':
+          setQuizFile("/mysql22.pdf");
+          break;
+        case 'mysql3':
+          setQuizFile("/mysql23.pdf");
+          break;
+        case 'mysql4':
+          setQuizFile("/mysql24.pdf");
+          break;
+        case 'mysql5':
+          setQuizFile("/mysql25.pdf");
+          break;
+        case 'mysql6':
+          setQuizFile("/mysql26.pdf"); // Fixed typo
+          break;
+        case 'mysql7':
+          setQuizFile("/mysql27.pdf");
+          break;
+        default:
+          setQuizFile("/mysql28.pdf");
+      }
+    }, [selectedQuiz]);
     return (
         <div className="quiz-selector">
             <select id="quiz-select" value={selectedQuiz} onChange={handleQuizChange}>
@@ -10,6 +42,17 @@ export default function QuizSelector({ selectedQuiz, handleQuizChange }) {
                 <option value="mysql6">Машини на състоянието</option>
                 <option value="mysql7">Машини на състоянието - варианти</option>
             </select>
+   
+        <a 
+          href={quizFile} 
+          download={quizFile.split('/').pop()} // Extracts the file name from the path
+          className="download-btn"
+        >
+          <FaDownload /> 
+        </a>
+
+            
         </div>
+        
     )
 }
