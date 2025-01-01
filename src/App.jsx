190f09806708ab54a486efa383abdc7f3cbcb1d3
nina <nina_nikolova@students.softuni.bg>
2025-01-01 14:40:16 +0200

    corrected quiz

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import Quiz from './components/Quiz1/Quiz';
 import Quiz2 from './components/Quiz2/Quiz2';
 import Quiz3 from './components/Quiz3/Quiz3';
 import Quiz4 from './components/Quiz4/Quiz4';
+import UnderConstruction from './components/UnderConstruction';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        {/* <Route path="/" element={<UnderConstruction />} /> */}
         <Route path="/quiz" element={<Quiz />} />
         <Route path="/quiz2" element={<Quiz2 />} />
         <Route path="/quiz3" element={<Quiz3 />} />
---
+++ src/components/UnderConstruction.jsx
@@ -0,0 +1,8 @@
+const UnderConstruction = () => (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>🚧 Under Construction 🚧</h1>
+      <p>This feature is under development. Stay tuned!</p>
+    </div>
+  );
+  
+  export default UnderConstruction;
