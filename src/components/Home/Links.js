a3a08bc481dfbf799530d54eed708eb05bc19c63
nina <nina_nikolova@students.softuni.bg>
2025-05-29 10:09:36 +0300

    delete old subjects

--- src/components/Home/Links.js
+++ src/components/Home/Links.js
@@ -1,7 +1,6 @@
 export const quizLinks = [
-    { path: "/quiz5", imgSrc: "/upp.png", alt: "Quiz Icon", text: "Управление на проекти" },
     { path: "/quiz6", imgSrc: "/dm.png", alt: "Quiz Icon", text: "Data mining" },
-    { path: "/quiz", imgSrc: "/sp.png", alt: "PAIP", text: "ССБУ" },
+    { path: "/quiz", imgSrc: "/sp.png", alt: "SSBU", text: "ССБУ" },
     // { path: "/quiz2", imgSrc: "/sm.png", alt: "Quiz Icon", text: "Модели на софт.системи" },
     // { path: "/quiz3", imgSrc: "/arch.png", alt: "Quiz Icon", text: "Архитектури на софт.с-ми" },
     // { path: "/quiz4", imgSrc: "/sp.png", alt: "Quiz Icon", text: "Софт.техн.advance" }
